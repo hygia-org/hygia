0b4d22bba976d6cc86ec9a8e597c29032b48bd98
atarchetti <atarchetti@avantstay.com>
2022-12-29 20:33:56 -0300

    #15 create class for tests

--- tests/data_pipeline/feature_engineering/test_key_smash.py
+++ tests/data_pipeline/feature_engineering/test_key_smash.py
@@ -1,25 +1,28 @@
 import pytest
 from src.data_pipeline.feature_engineering.key_smash import KeySmash
 
-key_smash = KeySmash()
+class TestKeySmash:
+    
+    def setup_method(self):
+        self.key_smash = KeySmash()
 
-@pytest.mark.parametrize("data, expected_output", [
-    ("PUENTECILLA KM. 1.7", 1.121212121212121),
-    ("ASDASD XXXX", 3.0)
-])
-def test_char_frequency_metric(data, expected_output):
-    assert key_smash.calculate_char_frequency_metric(data) == expected_output
+    @pytest.mark.parametrize("data, expected_output", [
+        ("PUENTECILLA KM. 1.7", 1.121212121212121),
+        ("ASDASD XXXX", 3.0)
+    ])
+    def test_char_frequency_metric(self, data, expected_output):
+        assert self.key_smash.calculate_char_frequency_metric(data) == expected_output
 
-@pytest.mark.parametrize("data, opt, expected_output", [
-    ("PUENTECILLA KM. 1.7", "vowels", 0.21052631578947367),
-    ("ASDASD XXXX", "consonants", 2.1818181818181817)
-])
-def test_irregular_sequence_metric(data, opt, expected_output):
-    assert key_smash.calculate_irregular_sequence_metric(data, opt) == expected_output
+    @pytest.mark.parametrize("data, opt, expected_output", [
+        ("PUENTECILLA KM. 1.7", "vowels", 0.21052631578947367),
+        ("ASDASD XXXX", "consonants", 2.1818181818181817)
+    ])
+    def test_irregular_sequence_metric(self, data, opt, expected_output):
+        assert self.key_smash.calculate_irregular_sequence_metric(data, opt) == expected_output
 
-@pytest.mark.parametrize("data, expected_output", [
-    ("ABC 123 !@#", 0.0),
-    ("ABC123 !@#", 0.9)
-])
-def test_number_count_metric(data, expected_output):
-    assert key_smash.calculate_number_count_metric(data) == expected_output
+    @pytest.mark.parametrize("data, expected_output", [
+        ("ABC 123 !@#", 0.0),
+        ("ABC123 !@#", 0.9)
+    ])
+    def test_number_count_metric(self, data, expected_output):
+        assert self.key_smash.calculate_number_count_metric(data) == expected_output
