eb2b6c10337cee6348fc3c7771336e5fd55970dc
Carla Rocha <seu_email@exemplo.com>
2023-01-07 12:12:10 -0300

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,8 +25,6 @@ import sphinx_rtd_theme
 
 
 
-
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -63,5 +61,5 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_theme_path = ["_themes", ]
-html_static_path = ['_static']
+
 
