f0b8eab25c37ad024f27143335bbe729ef138c6c
atarchetti <atarchetti@avantstay.com>
2023-01-14 23:24:46 -0300

    #17 update docstring for sphinix format in KeySmash class

--- src/data_pipeline/feature_engineering/key_smash.py
+++ src/data_pipeline/feature_engineering/key_smash.py
@@ -1,10 +1,21 @@
 from statistics import mean
 
 class KeySmash:
-    """A class for calculating metrics to indicate key smashing behavior in a text.
-    
+    """
+    A class for calculating metrics to indicate key smashing behavior in a text.
+
     Key smashing is the act of typing on a keyboard in a rapid and uncontrolled manner,
     often resulting in a series of random characters being entered into a document or text field.
+
+    Examples
+    --------
+    Use this class like this:
+
+    .. code-block:: python
+
+        keysmash = KeySmash()
+        df = keysmash.extract_key_smash_features(df, "text_column")
+        print(df)
     """
     
     
@@ -15,26 +26,28 @@ class KeySmash:
             "special_characters": '!@#$%^¨|\'\"&*()_+:;~`´]}{[}ºª=-.¿¡'
         }
     
-    def calculate_char_frequency_metric(self, text):
+    def calculate_char_frequency_metric(self, text:str) -> float:
         """
         Calculate the Char Frequency Metric.
-        
-        Parameters
-        ----------
-        text : str
-            The text to use for the calculation.
-            
-        Returns
-        -------
-        float
-            Char Frequency Metric.
-            
+
+        :param text: The text to use for the calculation.
+        :type text: str
+        :return: The calculated Char Frequency Metric.
+        :rtype: float
+
         Examples
         --------
-        >>> calculate_char_frequency_metric("PUENTECILLA KM. 1.7")
-        1.121212121212121
-        >>> calculate_char_frequency_metric("ASDASD XXXX")
-        3.0
+        Use this function like this:
+
+        .. code-block:: python
+
+            res = calculate_char_frequency_metric("PUENTECILLA KM. 1.7")
+            print(res)
+            # Output: 1.121212121212121
+
+            res = calculate_char_frequency_metric("ASDASD XXXX")
+            print(res)
+            # Output: 3.0
         """
         word_results = []
 
@@ -50,31 +63,34 @@ class KeySmash:
         else:
             return mean(word_results)
     
-    def calculate_irregular_sequence_metric(self, text, opt):
+    def calculate_irregular_sequence_metric(self, text:str, opt:str) -> float:
         """
         Calculate the Irregular Sequence Metric.
         
-        Parameters
-        ----------
-        text : str
-            The text to use for the calculation.
-        opt : str
-            The type of characters to consider for the calculation,
-            can be one of 'vowels', 'consonants', or 'special_characters'.
-            
-        Returns
-        -------
-        float
-            Irregular Sequence Metric.
-            
+        :param text: The text to use for the calculation.
+        :type text: str
+        :param opt: The type of characters to consider for the calculation, can be one of 'vowels', 'consonants', or 'special_characters'.
+        :type opt: str
+        :return: The calculated Irregular Sequence Metric.
+        :rtype: float
+
         Examples
         --------
-        >>> calculate_irregular_sequence_metric("PUENTECILLA KM. 1.7", "vowels")
-        0.21052631578947367
-        >>> calculate_irregular_sequence_metric("ASDASD XXXX", "consonants")
-        2.1818181818181817
-        >>> calculate_irregular_sequence_metric("!@#$% ASDFGHJKL", "special_characters")
-        1.5625
+        Use this function like this:
+
+        .. code-block:: python
+
+            res = calculate_irregular_sequence_metric("PUENTECILLA KM. 1.7", "vowels")
+            print(res)
+            # Output: 0.21052631578947367
+
+            res = calculate_irregular_sequence_metric("ASDASD XXXX", "consonants")
+            print(res)
+            # Output: 2.1818181818181817
+
+            res = calculate_irregular_sequence_metric("!@#$% ASDFGHJKL", "special_characters")
+            print(res)
+            # Output: 1.5625
         """
         count_sequence = 1
         sequence_regex = []
@@ -95,26 +111,28 @@ class KeySmash:
             
         return sum(sequence_regex)/len(text)
     
-    def calculate_number_count_metric(self, text):
+    def calculate_number_count_metric(self, text:str) -> float:
         """
-        Calculate the Number Count Metric.
-        
-        Parameters
-        ----------
-        text : str
-            The text field to use for the calculation.
-            
-        Returns
-        -------
-        float
-            Number Count Metric.
-            
+        Calculate the Number Count Metric for a given text.
+
+        :param text: The text to extract the metric from.
+        :type text: str
+        :return: The calculated Number Count Metric.
+        :rtype: float
+
         Examples
         --------
-        >>> calculate_number_count_metric("ABC 123 !@#")
-        0.0 
-        >>> calculate_number_count_metric("ABC123 !@#")
-        0.9
+        Use this function like this:
+
+        .. code-block:: python
+
+            res = calculate_number_count_metric("ABC 123 !@#")
+            print(res)
+            # Output: 0.0
+
+            res = calculate_number_count_metric("ABC123 !@#")
+            print(res)
+            # Output: 0.9
         """
         text_list = text.split()
         calc_num_line = 0
@@ -129,7 +147,7 @@ class KeySmash:
             return calc_num_line / len(' '.join(text_list))
         return 0
     
-    def extract_key_smash_features(self, df, column_name, normalize=True):
+    def extract_key_smash_features(self, df:pd.DataFrame, column_name:str, normalize:bool=True) -> pd.DataFrame:
         """
         Extract key smash features from a given dataframe and column.
 
@@ -139,9 +157,21 @@ class KeySmash:
         :type column_name: str
         :param normalize: Indicates whether to normalize the key smash feature columns. Default is True.
         :type normalize: bool, optional
-        
+
         :return: The input dataframe with additional columns for key smash features: 'irregular_sequence_vowels', 'irregular_sequence_consonants', 'irregular_sequence_special_characters', 'number_count_metric', 'char_frequency_metric'
         :rtype: pandas.DataFrame
+
+        Examples
+        --------
+        Use this function like this:
+
+        .. code-block:: python
+
+            import pandas as pd
+            keysmash = KeySmash()
+            df = pd.DataFrame({"text_column": ["abcdefgh", "ijklmnop", "qrstuvwxyz"]})
+            df = keysmash.extract_key_smash_features(df, "text_column", normalize=False)
+            print(df.head())
         """
         df['irregular_sequence_vowels'] = df[column_name].fillna('').apply(lambda x: self.calculate_irregular_sequence_metric(x, 'vowels'))
         df['irregular_sequence_consonants'] = df[column_name].fillna('').apply(lambda x: self.calculate_irregular_sequence_metric(x, 'consonants'))
