d7858c5e72c80a9ef125c735b0b1ff8dbc68c728
atarchetti <atarchetti@avantstay.com>
2023-01-23 20:09:21 -0300

    #51 refactor examples and add retrain model method

--- hygia/data_pipeline/model/random_forest.py
+++ hygia/data_pipeline/model/random_forest.py
@@ -1,5 +1,8 @@
 import pickle
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+import pandas as pd
 
 class RandomForestModel:
   def __init__(self, model_file=None, n_estimators=100, max_depth=None, random_state=0):
@@ -12,6 +15,26 @@ class RandomForestModel:
       self.max_depth = max_depth
       self.random_state = random_state
       self.model = RandomForestClassifier(n_estimators=self.n_estimators, max_depth=self.max_depth, random_state=self.random_state)
+  
+  def train_and_get_scores(self, df, concatened_column_name, all_features_columns, test_size=0.3):
+    df_not_duplicates = df.drop_duplicates(subset=[concatened_column_name])
+    
+    KEY_SMASH_COUNT = df_not_duplicates['target'].value_counts()['key_smash']
+    df_valid = df_not_duplicates[df_not_duplicates['target'] == 'valid'].sample(n=KEY_SMASH_COUNT, random_state=42)
+    df_invalid = df_not_duplicates[df_not_duplicates['target'] != 'valid']
+    df_balanced = pd.concat([df_valid, df_invalid])
+
+    df_balanced['is_key_smash'] = df_balanced['target'] == 'key_smash'
+
+    X = df_balanced[[*all_features_columns]]
+    y = df_balanced['is_key_smash']
+
+    clf=self.model
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    clf.fit(X_train,y_train)
+    scores = cross_val_score(clf, X, y, cv=5)
+    return clf, scores
   
   def predict(self, X):
     return self.model.predict(X)
