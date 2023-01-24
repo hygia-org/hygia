94a67a8d7676d03fffe8fd9f6d5b0c74de57c012
atarchetti <atarchetti@avantstay.com>
2023-01-24 11:23:46 -0300

    #51 new predicted model with regex features

--- hygia/data_pipeline/model/random_forest.py
+++ hygia/data_pipeline/model/random_forest.py
@@ -35,8 +35,11 @@ class RandomForestModel:
     clf=self.model
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    
     clf.fit(X_train,y_train)
+    
     y_pred = clf.predict(X_test)
+    
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
