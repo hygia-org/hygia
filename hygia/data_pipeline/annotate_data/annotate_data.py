8cfcf74a0274a8109762d8453dc4d140daa16ab0
Isaque Alves <isaquealvesdl@gmail.com>
2023-02-07 13:43:38 -0300

    #68 documenting annotate_data classes and functions

--- hygia/data_pipeline/annotate_data/annotate_data.py
+++ hygia/data_pipeline/annotate_data/annotate_data.py
@@ -2,8 +2,43 @@ import pandas as pd
 from colorama import Fore, Style
 
 class AnnotateData:
+    """
+    A class to incorporate the data annotation phase, starting from the thresholds 
+    (e.g., count sequence squared vowels, count sequence squared consonants) can tell if it's a ksmash.
 
+    Examples
+    --------
+    Use this class like this:
+
+    .. code-block:: python
+
+        annotate_data = hg.AnnotateData()
+        key_smash_thresholds = {
+        'count_sequence_squared_vowels': 1.00,
+        'count_sequence_squared_consonants': 1.999,
+        'count_sequence_squared_special_characters': 2.2499,
+        'ratio_of_numeric_digits_squared': 2.9,
+        'average_of_char_count_squared': 2.78,
+        }
+
+        df = annotate_data.annotate_data(df, concatened_column_name, key_smash_thresholds)
+        print(df)
+    """
     def annotate_data(self, df, concatened_column_name, ks_thresholds):
+        """
+        Annotate data function.
+        
+        :param df: Dataframe to extract features from.
+        :type df: pandas.DataFrame
+        :param concatened_column_name: Dataframe column to be used
+        :type concatened_column_name: List
+        :param ks_thresholds: List of thresholds
+        :type ks_thresholds: List
+
+        :return: The input dataframe with additional columns for key smashing and word embedding features.
+        :rtype: pandas.DataFrame
+        """
+
         print(f'{Fore.YELLOW}running annotate data with configs below...{Fore.WHITE}')
         
         print(f'{Style.BRIGHT}thresholds -> {Style.NORMAL}{ks_thresholds}')
