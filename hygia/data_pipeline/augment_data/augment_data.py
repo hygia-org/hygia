a3d6a2aa6832f14917be67f7315797bae7561ba0
Isaque Alves <isaquealvesdl@gmail.com>
2023-02-07 13:42:02 -0300

    #68 documenting data_pipeline classes and functions

--- hygia/data_pipeline/augment_data/augment_data.py
+++ hygia/data_pipeline/augment_data/augment_data.py
@@ -3,14 +3,19 @@ from hygia.paths.paths import root_path
 
 class AugmentData:
     """
-    Zipcode:
-        validations based on zipocde data from this website:
+        This class present a validations based on zipocde data from this website:
         https://www.listendata.com/2020/11/zip-code-to-latitude-and-longitude.html?m=1
         we downloaded data from some continents and we filter based in the 'country' code
-        we saved the data as pickle files in order to not overwhelm git history
+        we saved the data as pickle files in order to not overwhelm git history.
     """
     
     def __init__(self, country:str) -> None:
+        """
+        Initialize the AugmentData class.
+        
+        :param country: Zipcode list of the region or country used.
+        :type country: str
+        """
         continent_files = {
             'north_america': 'zip_to_lat_lon_North America.pkl',
             'south_america': 'zip_to_lat_lon_South America.pkl'
@@ -30,9 +35,30 @@ class AugmentData:
         self.country_zipcode_df = country_zipcode_df_raw.drop_duplicates(subset=['postal code'])
     
     def validate_zipcode(self, text:str) -> bool:
+        """
+        Check if a zipcode is valid.
+        
+        :param text: Zipcode list of the region or country used.
+        :type text: str
+
+        :return: Return if the zipcode is valid
+        :rtype: bool
+        """
         return text in self.country_zipcode_df['postal code'].values
     
     def validate_zipcodes(self, df:pd.DataFrame, zipcode_column_name:str) -> pd.DataFrame:
+        """
+        Check if all zipcode in a data is valid.
+        
+        :param df: Dataframe to extract features from.
+        :type df: pandas.DataFrame
+
+        :param zipcode_column_name: Zipcode column name
+        :type zipcode_column_name: str
+
+        :return: Return a dataframe with a new column.
+        :rtype: DataFrame
+        """
         if zipcode_column_name not in df:
             return
         validated_column = f"{zipcode_column_name}_is_valid"
@@ -42,6 +68,18 @@ class AugmentData:
         return df_aux[[validated_column]]
     
     def augment_data(self, df:pd.DataFrame, zipcode_column_name:str) -> pd.DataFrame:
+        """
+        Function that uses the validate_zipcodes function and concatenates the result to the database
+        
+        :param df: Dataframe to extract features from.
+        :type df: pandas.DataFrame
+
+        :param zipcode_column_name: Zipcode column name
+        :type zipcode_column_name: str
+
+        :return: Return a dataframe with a new column.
+        :rtype: DataFrame
+        """
         df = pd.concat([df, self.validate_zipcodes(df, zipcode_column_name)], axis=1)
         return df
     
--- hygia/data_pipeline/pre_process_data/pre_process_data.py
+++ hygia/data_pipeline/pre_process_data/pre_process_data.py
@@ -4,7 +4,27 @@ from colorama import Style
 from hygia.paths.paths import root_path
 
 class PreProcessData:
+    """
+    This class presents a series of functions that help in data pre-processing.
+    As concatenate columns, replace abbreviation, and etc.
+
+
+    Examples
+    --------
+    Use this class like this:
+
+    .. code-block:: python
+        pre_process_data = hg.PreProcessData()
+        df = pre_process_data.pre_process_data(df, ['COLUMN_1', 'COLUMN_2'], concatened_column_name)
+        print(df)
+    """
     def __init__(self, country:str=None, abbreviations_file:str=None) -> None:
+        """
+        Initialize the PreProcessData class.
+        
+        :param country: Zipcode list of the region or country used.
+        :type country: str
+        """
         self.abbreviations_dict = {}
         if not country and not abbreviations_file:
             return
@@ -21,12 +41,36 @@ class PreProcessData:
                 self.abbreviations_dict.update({key: value})
     
     def concatenate_columns(self, df, columns, concatenated_column_name):
+        """
+        Function that concatenates two columns and saves in a new one, whose name is informed by the user.
+        
+        :param df: Dataframe.
+        :type df: pandas.DataFrame
+
+        :param columns: List of columns
+        :type columns: List
+
+        :param concatenated_column_name: Name of the new column
+        :type concatenated_column_name: str
+
+        :return: Return the columns concatenated
+        """
+
         print(f'aliases indified: {Style.BRIGHT}{concatenated_column_name} -> {Style.NORMAL}{columns}')
         
         df[concatenated_column_name] = df[columns].astype(str).agg(' '.join, axis=1)
         return df
     
     def handle_nulls(self, df, column_name):
+        """
+        Handle null values
+        
+        :param df: Dataframe
+        :type df: pandas.DataFrame
+
+        :param column_name: Column name to check
+        :type column_name: str
+        """
         print(f'handle null values in the column {Style.BRIGHT}{column_name}{Style.NORMAL}')
         
         df[column_name].fillna('').astype(str)
@@ -37,16 +81,46 @@ class PreProcessData:
         return df
     
     def _replace_abbreviation(self, text:str) -> str:
+        """
+        Function that identifies abbreviations and according to the dictionary changes the names
+        
+        :param text: Text to be analyzed
+        :type text: str
+        """
         for abbreviation in self.abbreviations_dict:
             text = ' '.join([re.sub(rf'(\b|(?<=[^a-zA-Z])){abbreviation}(\.|\b|(?=[^a-zA-Z]))', self.abbreviations_dict[abbreviation], e, flags=re.IGNORECASE) for e in text.split()])
         return text
     
     def handle_abreviations(self, df, column_name):
+        """
+        Handles abbreviations in the dataframe
+        
+        :param df: Dataframe
+        :type df: DataFrame
+
+        :param column_name: Column name to check
+        :type column_name: str
+        """
+
         df[column_name] = df[column_name].apply(lambda x: self._replace_abbreviation(x))
         return df
     
     def pre_process_data(self, df, columns_to_concat=None, column_name=None):
+        """
+        Function that gathers all implemented preprocessing (column concatenation, handle with nulls and abbreviations)
         
+        :param df: Dataframe
+        :type df: DataFrame
+
+        :param columns_to_concat: List of columns
+        :type columns_to_concat: List
+
+        :param column_name: Column name to check
+        :type column_name: str
+
+        :return: The input dataframe with additional columns
+        :rtype: pandas.DataFrame
+        """
         if columns_to_concat and column_name:
             df = self.concatenate_columns(df, columns_to_concat, column_name)
         
