f713c26cc793b78bdbf7eba4b53cc7150156e622
Ana Hoffmann <ana.luiza.hoff@gmail.com>
2023-06-19 16:53:30 -0300

    #105 Adding tests to YAML parser

--- tests/parser/test_YAML_parser.py
+++ tests/parser/test_YAML_parser.py
@@ -1,4 +1,3 @@
-
 from hygia.parser.YAML_parser import YAMLParser
 
 class TestYamlParser():
@@ -22,8 +21,36 @@ class TestYamlParser():
     def test_yaml_has_feature_settings(self):
         assert 'output_folder' in self.yaml
         assert self.yaml['output_folder'] == 'output'
-        
+    
     def test_yaml_has_feature_settings(self):
         assert 'feature_engineering' in self.yaml
         assert type(self.yaml['feature_engineering']) == list
         assert 'input' in self.yaml['feature_engineering'][0]
+    
+    def test_yaml_has_model(self):
+        assert 'model' in self.yaml
+        assert self.yaml['model'] == 'model'
+
+    def test_yaml_has_nrows(self):
+        assert 'nrows' in self.yaml
+    
+    def test_yaml_has_engine(self):
+        assert 'engine' in self.yaml
+
+    def test_yaml_has_encoding(self):
+        assert 'encoding' in self.yaml
+
+    def test_yaml_has_separator(self):
+        assert 'separator' in self.yaml
+
+    def test_yaml_has_model(self):
+        assert 'model' in self.yaml
+        assert self.yaml['model'] == 'model'
+
+    def test_yaml_has_annotate_data(self):
+        assert 'annotate_data' in self.yaml
+        assert self.yaml['annotate_data'] == 'annotate_data'
+
+    def test_yaml_has_preprocessing(self):
+        assert 'pre_processing' in self.yaml
+        assert self.yaml['pre_processing'] == 'pre_processing'
